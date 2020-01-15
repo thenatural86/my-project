14b118640f4f7d2d5ae307fd5f59d2f093ed00eb
thenatural86 <philip.smalls86@gmail.com>
2020-01-15 18:13:06 -0500

    native-state

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
 import React, { Component } from "react"
 import { Text, View } from "react-native"
 
-class Greeting extends Component {
-  render() {
-    return (
-      <View style={{ alignItems: "center" }}>
-        <Text>Hello {this.props.name}</Text>
-      </View>
+class Blink extends Component {
+  state = { isShowingText: true }
+
+  componentDidMount() {
+    setInterval(
+      () =>
+        this.setState(previousState => ({
+          isShowingText: !previousState.isShowingText
+        })),
+      1000
     )
+  }
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null
+    }
+    return <Text>{this.props.text}</Text>
   }
 }
 
-export default class LotsOfGreetings extends Component {
+export default class BlinkApp extends Component {
   render() {
     return (
-      <View style={{ alignItems: "center", top: 50 }}>
-        <Greeting name="jame" />
-        <Greeting name="joe" />
-        <Greeting name="nancy" />
+      <View style={{ alignItems: "center", top: 200 }}>
+        <Blink text="lets go" />
       </View>
     )
   }
